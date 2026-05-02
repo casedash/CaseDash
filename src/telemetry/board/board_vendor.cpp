8660fe3ab8699f4dc06d35823157fb721155ab3e
Roman Elizarov <elizarov@gmail.com>
2026-05-02 10:57:09 +0300

    Add MSI Center board telemetry provider

--- src/telemetry/board/board_vendor.cpp
+++ src/telemetry/board/board_vendor.cpp
@@ -1,8 +1,26 @@
 #include "telemetry/board/board_vendor.h"
 
 #include "telemetry/board/gigabyte/board_gigabyte_siv.h"
+#include "telemetry/board/msi/board_msi_center.h"
+#include "telemetry/impl/system_info_support.h"
+#include "util/strings.h"
 #include "util/trace.h"
 
+namespace {
+
+constexpr wchar_t kBiosKey[] = L"HARDWARE\\DESCRIPTION\\System\\BIOS";
+
+}  // namespace
+
 std::unique_ptr<BoardVendorTelemetryProvider> CreateBoardVendorTelemetryProvider(Trace& trace) {
+    const std::string manufacturer =
+        ReadRegistryString(HKEY_LOCAL_MACHINE, kBiosKey, L"BaseBoardManufacturer").value_or("");
+    if (ContainsInsensitive(manufacturer, "micro-star") || ContainsInsensitive(manufacturer, "msi")) {
+        return CreateMsiBoardTelemetryProvider(trace);
+    }
+    if (ContainsInsensitive(manufacturer, "gigabyte")) {
+        return CreateGigabyteBoardTelemetryProvider(trace);
+    }
+
     return CreateGigabyteBoardTelemetryProvider(trace);
 }
