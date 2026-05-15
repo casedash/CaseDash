ea8f320482ccf7a4a7d749d1be54ba43f184ebef
Roman Elizarov <elizarov@gmail.com>
2026-05-16 00:44:13 +0300

    Add current machine hardware to vendor selection tests

--- tests/hardware_vendor_selection_tests.cpp
+++ tests/hardware_vendor_selection_tests.cpp
@@ -17,10 +17,12 @@ struct KnownTestBoard {
 
 const KnownTestGpu kKnownTestGpus[] = {
     {GpuVendorInfo{0x1002u, "AMD Radeon RX 6800"}, GpuVendor::Amd},
+    {GpuVendorInfo{0x10deu, "NVIDIA GeForce RTX 3080"}, GpuVendor::Nvidia},
 };
 
 const KnownTestBoard kKnownTestBoards[] = {
     {BoardVendorInfo{"Gigabyte Technology Co., Ltd.", "X570 I AORUS PRO WIFI"}, BoardVendor::Gigabyte},
+    {BoardVendorInfo{"Micro-Star International Co., Ltd.", "MPG Z690 CARBON WIFI (MS-7D30)"}, BoardVendor::Msi},
 };
 
 }  // namespace
